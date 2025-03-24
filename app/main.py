5f8bec7d7f7021a19bebcc8556b46101ba60d103
Yahia Salman <yahia.salman.04@gmail.com>
2025-03-24 15:40:14 -0400

    fixed git ignore

--- app/main.py
+++ app/main.py
@@ -1,3 +1,15 @@
 from fastapi import FastAPI
+from app.schemas.auth import UserLogin
+
 
 app = FastAPI()
+
+@app.post("/login")
+def login(user_login: UserLogin):
+    print(user_login.email)
+    print(user_login.password)
+
+
+@app.post("/register")
+def register(user_register: UserRegister):
+    pass
--- app/router/travel.py
+++ app/router/travel.py
@@ -1,3 +1,6 @@
 from fastapi import APIRouter, HTTPException, Depends
 
 app = APIRouter(prefix="/travel")
+
+app.post("/")
+
